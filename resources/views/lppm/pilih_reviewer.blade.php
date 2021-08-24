578f2820cc0685195b9e5f33d66ec1fd799294ba
bahrululum76 <bahrululum756@gmail.com>
2021-08-25 02:57:19 +0700

    up

--- resources/views/lppm/pilih_reviewer.blade.php
+++ resources/views/lppm/pilih_reviewer.blade.php
@@ -31,7 +31,7 @@
                 <tr class="thead">
                     
                     
-                    <td align="center"><a href="detailpilihreview/{{$proposal->id}}">{{$proposal->user->name}}</a></td>
+                    <td align="center"><a href="{{url('lppm/detailpilihreview/'.$proposal->id)}}">{{$proposal->user->name}}</a></td>
                     <td align="center">{{$proposal->status_id}}</td>
                     
                     
